bde3382494592b5fb3cecc9deb1ed254fcc85ce1
zhangxin5 <zhangxin5@kingsoft.com>
2015-09-12 12:18:24 +0800

    digest authentication

--- src/main/java/org/sandbox/config/WebSecurityConfig.java
+++ src/main/java/org/sandbox/config/WebSecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
 
 /**
  * Created by zhangxin on 15/9/10.
@@ -17,19 +22,48 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public DigestAuthenticationEntryPoint digestEntryPoint() {
+        DigestAuthenticationEntryPoint entryPoint = new DigestAuthenticationEntryPoint();
+        entryPoint.setRealmName("Custom Realm Name");
+        entryPoint.setKey("uniqueAndSecret");
+        return entryPoint;
+    }
+
+    @Bean
+    public DigestAuthenticationFilter digestFilter() throws Exception {
+        DigestAuthenticationFilter filter = new DigestAuthenticationFilter();
+        filter.setAuthenticationEntryPoint(digestEntryPoint());
+        filter.setUserDetailsService(userDetailsServiceBean());
+        return filter;
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/demo/v1/persons/**", "/demo/mongo/customers/**").permitAll()
-                .antMatchers("/demo/security/**").authenticated()
-                .and()
-                .httpBasic().realmName("spring123987");
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("zhangxin").password("123456").roles("USER_ROLE");
+                .withUser("zhangxin").password("123456").roles("USER_ROLE")
+                .and()
+                .withUser("test").password("test").roles("**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.exceptionHandling()
+                .authenticationEntryPoint(digestEntryPoint())
+                .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .antMatchers("/demo/v1/persons/**", "/demo/mongo/customers/**").permitAll()
+                .antMatchers("/demo/security/**").authenticated()
+                .and()
+            .addFilterAfter(digestFilter(), BasicAuthenticationFilter.class);
     }
 
     @Override
