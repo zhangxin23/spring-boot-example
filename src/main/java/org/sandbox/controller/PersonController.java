73d9bc5c1541325ac08bdb9b89eb0de3640d3439
zhangxin5 <zhangxin5@kingsoft.com>
2015-09-17 18:49:26 +0800

    delete several ApiResponse annotations

--- src/main/java/org/sandbox/controller/PersonController.java
+++ src/main/java/org/sandbox/controller/PersonController.java
@@ -26,16 +26,15 @@ public class PersonController {
 
     @RequestMapping(value = "persons/{id}", method = RequestMethod.GET)
     @ResponseBody
-    @ApiOperation(value = "Fetch API", notes = "Fetch person by id", response = Void.class)
+    @ApiOperation(value = "Fetch API", notes = "Fetch person by id", response = Person.class)
     @ApiResponses(value = {@ApiResponse(code = 500, message = "Server Error", response = ErrorDetail.class)})
     public Object getPerson(@PathVariable int id) {
         return personService.select(id);
     }
 
     @RequestMapping(value = "persons", method = RequestMethod.POST)
-    @ApiOperation(value = "POST API", notes = "Add new person by multiple params into database", response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "add successfully", response = String.class),
-                           @ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
+    @ApiOperation(value = "POST API", notes = "Add new person by multiple params into database", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
     public ResponseEntity<String> setPerson(@RequestParam("name")String name,
                             @RequestParam("age")int age,
                             @RequestParam("country")String country) {
@@ -52,20 +51,18 @@ public class PersonController {
         return new ResponseEntity<>("ResponseEntity insert successfully", headers, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "personObject", method = RequestMethod.POST)
+    @RequestMapping(value = "persons/object", method = RequestMethod.POST)
     @ResponseBody
-    @ApiOperation(value = "Set API", notes = "Add new person by person object into database", response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "add successfully", response = String.class),
-                           @ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
+    @ApiOperation(value = "Set API", notes = "Add new person by person object into database", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
     public Object setPersonObject(@RequestBody Person person) {
         personService.insert(person);
         return "insert successfully.";
     }
 
     @RequestMapping(value = "persons/{id}", method = RequestMethod.PUT)
-    @ApiOperation(value = "Update API", notes = "Update person by id and multiply params", response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "update successfully", response = String.class),
-                           @ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
+    @ApiOperation(value = "Update API", notes = "Update person by id and multiply params", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
     public ResponseEntity<String> updatePerson(@PathVariable("id") int id, @RequestParam("name") String name,
                                                @RequestParam("age") int age,
                                                @RequestParam("country") String country) {
@@ -80,10 +77,9 @@ public class PersonController {
         return new ResponseEntity<>("update successfully", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "personObject/{id}", method = RequestMethod.PUT)
-    @ApiOperation(value = "Update API", notes = "Update person by id and person object params into database", response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "add successfully", response = String.class),
-                           @ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
+    @RequestMapping(value = "persons/object/{id}", method = RequestMethod.PUT)
+    @ApiOperation(value = "Update API", notes = "Update person by id and person object params into database", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
     public ResponseEntity<String> updatePersonObject(@PathVariable("id") int id, @RequestBody Person person) {
         person.setId(id);
         personService.update(person);
@@ -93,8 +89,7 @@ public class PersonController {
 
     @RequestMapping(value = "persons/{id}", method = RequestMethod.DELETE)
     @ApiOperation(value = "Delete API", notes = "Delete person by id", response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "delete successfully", response = Void.class),
-                           @ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "server error", response = ErrorDetail.class)})
     public ResponseEntity<?> delPerson(@PathVariable int id) {
         personService.delete(id);
         return new ResponseEntity<>(null, HttpStatus.OK);
